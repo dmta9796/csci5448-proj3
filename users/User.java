1ace1ccee40abad8cce53bd80c6312bb0063042c
willculkin <wicu6711@colorado.edu>
2020-02-26 16:42:19 -0700

    added current rented cars to User

--- users/User.java
+++ users/User.java
@@ -1,14 +1,18 @@
 package users;
+import java.util.List;
+import automobiles.AbstractCar;
+
 public class User{
     private String name = "";
-    private
-    BuyBehavior clienttype;
+    private BuyBehavior clienttype;
+    private List<AbstractCar> curRentedCars;
     public User(){
         this.clienttype = new Casual();
     }
     public User(String name, BuyBehavior type){
         this.name = name;
         this.clienttype = type;
+        curRentedCars = null;
     }
     public String getname(){
         return this.name;
@@ -16,5 +20,8 @@ public class User{
     public void setname(String name){
         this.name = name;
     }
+
+    public List<AbstractCar> getCurRented(){return curRentedCars;};
+    public void setCurRentedCars( List<AbstractCar> newRental){curRentedCars = newRental;};
 
 }
