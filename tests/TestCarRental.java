377b25350cc30bdc3f5a2edfdf78fc7262d476c3
dmta9796 <dmta9796@colorado.edu>
2020-03-01 09:48:47 -0700

    added junit and class diagram.

---
+++ tests/TestCarRental.java
@@ -0,0 +1,12 @@
+package tests;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import automobiles.*;
+import ledger.*;
+import users.*;
+public class TestCarRental{
+    @Test
+    public void testRent(){
+        MyUnit myUnit = new MyUnit();
+    }
+}
